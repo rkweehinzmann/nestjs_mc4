51f3b2e2117040cc2ad89da93c9d731c1b5b3fde
Regina Hinzmann <regina.hinzmann@desy.de>
2024-09-04 10:48:17 +0200

    useGlobalflags works L27 OK

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
-    whitelist: true, // does not send undefined properties to our controller
-    forbidNonWhitelisted: true    // throws an error for each such undef prop
-    })
-  );
+    whitelist: true,
+    forbidNonWhitelisted: true,
+  }));
   await app.listen(3000);
 }
 bootstrap();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,20 +1,34 @@
-import { Controller, Get, Post, Param, Query, Body } from '@nestjs/common';
+import { Controller, Get, Post, Param, Query, Body,
+    Headers,
+    ParseIntPipe,
+    DefaultValuePipe,
+    ValidationPipe,
+ } from '@nestjs/common';
+import { OPTIONAL_PROPERTY_DEPS_METADATA } from '@nestjs/common/constants';
 import { CreateUserDto } from './dtos/create-users.dto.users';
-
 
 @Controller('users')
 export class UsersController {
     
-    @Get('/:id')
-    public getUsers(@Param() params: any, @Query() query: any){
-        console.log(params)
-        console.log(query)
+    @Get('/:id?')
+    public getUsers(
+
+        @Param('id', ParseIntPipe) id: number|undefined, 
+        @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+        @Query('page', new DefaultValuePipe(3), ParseIntPipe) page: number,
+        @Headers() myheader:any,
+    ){
+        console.log(id)
+        console.log(limit)
+        console.log(page)  
+        console.log(myheader)
         return 'You sent a GET request to users endpoint.'
     }
 
     @Post()
-    public createUsers(@Body() createUserDto:CreateUserDto){
-        console.log(CreateUserDto)
+    public createUsers(
+        @Body() createUserDto: CreateUserDto){
+        console.log(createUserDto)
         return 'You sent a POST request to users endpoint.'
     }
 
