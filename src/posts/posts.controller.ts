79388da3b3b2ea7ecb2dc4366c21fa94c7482b4e
Regina Hinzmann <regina.hinzmann@desy.de>
2024-09-04 15:44:24 +0200

    new posts module L38

---
+++ src/posts/posts.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { PostsService } from './providers/posts.service';
+
+@Controller('posts')
+export class PostsController {
+
+    constructor(
+        // Injecting Posts Service
+        private readonly postsService: PostsService,
+    ){}
+
+    // GET http://localhost:3000/posts/userId
+    @Get('/:userId?')
+    public getPosts(@Param('userId') userId:string) {
+        return this.postsService.findAll(userId)
+    }
+
+}
---
+++ src/posts/providers/posts.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class PostsService {
+    public findAll(userId: string){
+        console.log(userId)
+    }
+}
