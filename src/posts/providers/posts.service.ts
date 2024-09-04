ddaaab262e4762033defd687ee0d31d0c4ccc3ba
Regina Hinzmann <regina.hinzmann@desy.de>
2024-09-04 15:51:52 +0200

    L 40 return from posts

--- src/posts/providers/posts.service.ts
+++ src/posts/providers/posts.service.ts
@@ -4,5 +4,20 @@ import { Injectable } from '@nestjs/common';
 export class PostsService {
     public findAll(userId: string){
         console.log(userId)
+
+        // Users Service
+        // Find user
+
+        // find all his posts
+        return[
+            {
+                title: '1 Mo',
+                content: 'content A',
+            },
+            {
+                title: '2 Mo',
+                content: 'content B',
+            },
+        ]
     }
 }
