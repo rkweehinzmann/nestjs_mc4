994ba915a9aceab9f6da7bf6ec6c3f0fa9f7da44
Regina Hinzmann <regina.hinzmann@desy.de>
2024-10-07 11:48:43 +0200

    commit for debugging "Nest can't resolve dependencies of the UsersService"

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,23 @@ import { UsersController } from './users/users.controller';
 import { UsersService } from './users/providers/users.service';
 import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
 
 
 @Module({
-  imports: [UsersModule, PostsModule, AuthModule],
+  imports: [UsersModule, PostsModule, AuthModule, TypeOrmModule.forRootAsync({
+    useFactory: () => ({
+      type: 'postgres',
+      entities:[User],
+      synchronize: true, // never use in production mode!!
+      port: 5432,
+      username: 'postgres',
+      password: 'Jesus',
+      host: 'localhost',
+      database: 'nestjs-blog'
+    }), 
+  })],
   controllers: [AppController, UsersController],
   providers: [AppService, UsersService],
 })
--- src/users/providers/users.service.ts
+++ src/users/providers/users.service.ts
@@ -1,21 +1,37 @@
 import { Injectable, Inject, forwardRef } from '@nestjs/common';
 import { AuthService } from 'src/auth/providers/auth.service';
 import { GetUsersParamDto } from '../dtos/get-users-param.dto';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../user.entity';
+import { CreateUserDto } from '../dtos/create-users.dto';
 
 @Injectable()
 export class UsersService {
     
     constructor(
+/**
         // inject auth service
         @Inject( forwardRef(() => AuthService))
         private readonly authService: AuthService,
+*/
+
+        // inject repository
+        @InjectRepository(User)
+        private usersRepository:Repository<User>,
+    
     ){}
+
+    public async createUser(createUserDto: CreateUserDto){
+        // check if user already exists with this email
+    }
 
     public findAll(
      getUsersParamDto: GetUsersParamDto,
      limit: number,
      page: number,
 ){
+    /**
     const isAuth = this.authService.isAuth()
     console.log(isAuth)
     return [
@@ -28,7 +44,10 @@ export class UsersService {
           email: 'alice@doe.com',
         },
       ];
+
+    */
 }
+
     /*
     find user by Id
     */
